31d0ae265ed1342a8cfd42d4c7513ff894a9364f
TamerPC <tkabdolla@bk.ru>
2025-05-16 00:20:25 +0500

    [fix] NNet fix

--- searchgame/network/NNet.py
+++ searchgame/network/NNet.py
@@ -38,28 +38,3 @@ class NNetWrapper(NeuralNet):
 
     def load_checkpoint(self, folder, filename):
         self.nnet.model.load_weights(f"{folder}/{filename}.h5")
-
-# Coach.py
-from Coach import Coach
-from searchgame import SearchGame
-from NNet import NNetWrapper
-from utils import dotdict
-
-if __name__ == "__main__":
-    args = dotdict({
-        'size': 20,
-        'sorted': False,
-        'hidden': 128,
-        'lr': 0.001,
-        'batch_size': 64,
-        'epochs': 10,
-        'numMCTSSims': 50,
-        'numIters': 100,
-        'numEps': 20,
-        'cpuct': 1.0,
-        'checkpoint': './checkpoint',
-    })
-    game = SearchGame(args)
-    nnet = NNetWrapper(game, args)
-    c = Coach(game, nnet, args)
-    c.learn()
